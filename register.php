ed34383dd3658b9681c31af27a92af38d956e053
duronta <durontabangladesh@gmail.com>
2015-03-27 18:26:12 +0600

    Second commit after little modification

--- register.php
+++ register.php
@@ -12,17 +12,17 @@ if (version_compare(PHP_VERSION, '5.3.7', '<')) {
 require_once('config/config.php');
 
 // include the to-be-used language, english by default. feel free to translate your project and include something else
-require_once('translations/en.php');
+require_once('translations/bangla.php');
 
 // include the PHPMailer library
 require_once('libraries/PHPMailer.php');
 
 // load the registration class
-require_once('classes/Registration.php');
+require_once('classes/Level0RegistrationStepOne.php');
 
 // create the registration object. when this object is created, it will do all registration stuff automatically
 // so this single line handles the entire registration process.
 $registration = new Registration();
 
 // showing the register view (with the registration form, and messages/errors)
-include("views/register.php");
+include("views/level0RegistrationStepOne.php");
